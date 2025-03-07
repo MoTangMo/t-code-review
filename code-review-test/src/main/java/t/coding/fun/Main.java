886833d64c1f949296c27ea94f4c4364f121da14
T <www1532621280@qq.com>
2025-03-07 22:37:21 +0800

    添加测试类

--- code-review-test/src/main/java/t/coding/fun/Main.java
+++ code-review-test/src/main/java/t/coding/fun/Main.java
@@ -2,6 +2,9 @@ package t.coding.fun;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("添加测试类");
     }
+
+
+
 }
